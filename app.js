46fcd9f83c88679bf682737a69cd140cf80fdf24
vidar <vidars17@ru.is>
2020-11-14 02:04:00 +0000

    Bulma should now work as intended, will see if server handles it

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-	res.send("Hello world! :D")
+	res.render("home", {foo: 'FOO'});
 })
 
 app.listen(port, () => {
-	console.log(`App listening at https://viddi.dev:${port}`)
+	console.log(`App listening at https://viddi.dev:${port}`);
 })
