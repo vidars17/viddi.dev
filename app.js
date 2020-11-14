462c2decbf9eedbecd43106e507c4139f0709011
vidar <vidars17@ru.is>
2020-11-14 22:58:43 +0000

    Site is taking on a new look, very WIP

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const path = require('path');
+const favicon = require('serve-favicon');
+
+const port = process.env.APP_PORT;
+const host = process.env.APP_HOST;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-	res.render("home", {foo: 'FOO'});
+	res.render("index.ejs");
 })
-
+app.get("/about", (req, res) => {
+	res.render("about.ejs");
+})
+app.get("/cv", (req, res) => {
+	res.render("cv.ejs");
+})
 app.listen(port, () => {
-	console.log(`App listening at https://viddi.dev:${port}`);
+	console.log(`App listening at ${host}${port}`);
 })
